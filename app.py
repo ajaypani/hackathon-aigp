587b866fd970a0154db9b269bfad273a608e56a9
Ajay Panigrahi <79969922+ajaypani@users.noreply.github.com>
2023-06-25 13:06:40 +0100

    no unwanted comments

--- app.py
+++ app.py
@@ -21,47 +21,6 @@ Engage in a professional and informative manner, and avoid using jargon or compl
 Remember, your purpose is to assist users with their healthcare-related inquiries and provide educational support. With these guidelines in mind, proceed to help users with their questions, concerns, and medicine recommendations.
 """
 
-# TEMPERATURE = 0.5
-# MAX_TOKENS = 500
-# FREQUENCY_PENALTY = 0
-# PRESENCE_PENALTY = 0.6
-# # limits how many questions we include in the prompt
-# MAX_CONTEXT_QUESTIONS = 100
-
-# def get_response(instructions, previous_questions_and_answers, new_question):
-#     """Get a response from ChatCompletion
-
-#     Args:
-#         instructions: The instructions for the chat bot - this determines how it will behave
-#         previous_questions_and_answers: Chat history
-#         new_question: The new question to ask the bot
-
-#     Returns:
-#         The response text
-#     """
-#     # build the messages
-#     messages = [
-#         { "role": "system", "content": instructions },
-#     ]
-#     # add the previous questions and answers
-#     for question, answer in previous_questions_and_answers[-MAX_CONTEXT_QUESTIONS:]:
-#         messages.append({ "role": "user", "content": question })
-#         messages.append({ "role": "assistant", "content": answer })
-#     # add the new question
-#     messages.append({ "role": "user", "content": new_question })
-
-#     completion = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=messages,
-#         temperature=TEMPERATURE,
-#         max_tokens=MAX_TOKENS,
-#         top_p=1,
-#         frequency_penalty=FREQUENCY_PENALTY,
-#         presence_penalty=PRESENCE_PENALTY,
-#     )
-#     return completion.choices[0].message.content
-
-
 app = Flask(__name__, template_folder='templates')
 #app = Flask(_name_)
 
